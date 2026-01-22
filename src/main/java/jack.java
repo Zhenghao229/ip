47f97d44c62c3ba785f63b1ba36d1989d6d0d871
zhenghao <1376872538szh@gmail.com>
2026-01-23 00:24:31 +0800

    Level 2

--- src/main/java/jack.java
+++ src/main/java/jack.java
@@ -4,6 +4,10 @@ public class jack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // storage for tasks
+        String[] items = new String[100];
+        int count = 0;
+
         // Greeting
         System.out.println("Hello! I'm Jack.");
         System.out.println("What can I do for you?");
@@ -16,8 +20,18 @@ public class jack {
                 break;
             }
 
+            if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
+                continue;
+            }
+
+            items[count] = input;
+            count++;
+
             // Echo the input
-            System.out.println(input);
+            System.out.println("added:" + input);
         }
     }
 }
