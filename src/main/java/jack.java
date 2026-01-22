1628aca00f4e2b6eb035f0df3a63e5032b124d76
zhenghao <1376872538szh@gmail.com>
2026-01-22 22:58:29 +0800

    Level 0: rename chatbot, greet and exit

---
+++ src/main/java/jack.java
@@ -0,0 +1,7 @@
+public class jack {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Nova.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
