38edbd82c0d98681c1d281b92abb383a52a47d56
zhenghao <1376872538szh@gmail.com>
2026-02-18 22:20:22 +0800

    Level-10: GUI completed

--- src/main/java/jack/Main.java
+++ src/main/java/jack/Main.java
@@ -1,8 +1,6 @@
-package jack.ui;
+package jack;
 
-import java.io.IOException;
-
-import jack.Jack;
+import jack.ui.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,7 +8,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
- * A GUI for Duke using FXML.
+ * A GUI for Jack using FXML.
  */
 public class Main extends Application {
 
@@ -21,11 +19,15 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+
+            stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setJack(jack); // inject the Jack instance
+
             stage.show();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/jack/ui/DialogBox.java
+++ src/main/java/jack/ui/DialogBox.java
@@ -1,3 +1,5 @@
+package jack.ui;
+
 import java.io.IOException;
 import java.util.Collections;
 
